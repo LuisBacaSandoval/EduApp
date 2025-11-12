f1258aa60d7d949e0cfbe499fc9cd2cd5ceb7742
KevinPV11 <kpalominov@uni.pe>
2025-11-12 15:08:40 -0500

    correction main

--- server/main.py
+++ server/main.py
@@ -2,13 +2,12 @@
 Punto de entrada del servidor.
 Ejecuta la aplicación FastAPI usando uvicorn.
 """
-from app.main import app
 from app.core.config import settings
 import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run(
-        app,
+        "app.main:app",  # String de importación para habilitar reload
         host=settings.HOST,
         port=settings.PORT,
         reload=True  # Activa el modo desarrollo con recarga automática
